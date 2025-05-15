80e98c7a84fdf7f5c50ec0446b3adec77e38c6d8
Caio Marinho <Caiomarinho.silva17@gmail.com>
2025-05-15 19:48:58 -0300

    Update scraping.py

--- vitibrasil-api/app/scraping.py
+++ vitibrasil-api/app/scraping.py
@@ -1,7 +1,5 @@
 import requests
 from bs4 import BeautifulSoup
-from app.database.init_db import DB_PATH
-import sqlite3
 
 def extrair_dados(session, url):
     dados = []
@@ -72,15 +70,6 @@ def busca_categoria(ano: int, categoria: str):
                 titulo = dados.pop("titulo", None)
                 chave = titulo if titulo and titulo.lower() != "total" else sub
                 resultado[chave] = dados["linhas"]
-
-                # Gravar no banco
-                with sqlite3.connect(DB_PATH) as conn:
-                    cursor = conn.cursor()
-                    for item in dados["linhas"]:
-                        cursor.execute(
-                            "INSERT INTO dados (ano, categoria, dado) VALUES (?, ?, ?)",
-                            (ano, categoria, str(item))
-                        )
     else:
         url = f"http://vitibrasil.cnpuv.embrapa.br/index.php?ano={ano}&opcao={opcao}"
         dados = extrair_dados(session, url)
@@ -88,14 +77,5 @@ def busca_categoria(ano: int, categoria: str):
             titulo = dados.pop("titulo", None)
             chave = titulo if titulo and titulo.lower() != "total" else opcao
             resultado[chave] = dados["linhas"]
-
-            # Gravar no banco
-            with sqlite3.connect(DB_PATH) as conn:
-                cursor = conn.cursor()
-                for item in dados["linhas"]:
-                    cursor.execute(
-                        "INSERT INTO dados (ano, categoria, dado) VALUES (?, ?, ?)",
-                        (ano, categoria, str(item))
-                    )
 
     return resultado
