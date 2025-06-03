4afe50c88dc88be61670a2e73ab385b011301fe8
André Almeida <69471023+anndre45@users.noreply.github.com>
2025-06-03 18:20:49 -0300

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,3 @@ from app.utils.auth import check_auth
 from app.services.scraping import busca_categoria
 from app.core.app import app
 from app.routes import routes
-
-
-if __name__ == '__main__':
-    app.run(debug=True)
